bd7badcc8692340a56d4b705e5a85e83c6c814db
egasembiring <155513575+egasembiring@users.noreply.github.com>
2025-04-10 18:26:58 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,31 +1,8 @@
 import express from "express"
-import helmet from "helmet"
-import bodyParser from "body-parser"
 
-export default class Server {
-    constructor(port) {
+export default class Server{
+    constructor(port){
         this.server = express()
-        
-        // Security Middleware
-        this.server.use(helmet())
-        
-        // Body Parser
-        this.server.use(bodyParser.json())
-        this.server.use(bodyParser.urlencoded({ extended: true }))
-        
-        // Static Files
-        this.server.use(express.static('public'))
-        
-        // Basic Error Handling
-        this.server.use((err, req, res, next) => {
-            console.error(err.stack)
-            res.status(500).send('Server Error!')
-        })
-        
-        // Start Server
-        this.listen = this.server.listen(port, () => {
-            console.log(`Server running on port ${port}`)
-            console.log(`QR Auth: http://localhost:${port}/qr`)
-        })
+        this.listen = this.server.listen(port, () => console.log("Open http://localhost:"+port+"/qr to QR Auth"))
     }
 }
